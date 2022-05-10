c37670d6bead88d6841839feaaf845cf38b9fd67
srschoonmaker <srschoonmaker@gmail.com>
2022-05-10 17:59:58 -0400

    refactored behavior for generic views

--- restapi/tests.py
+++ restapi/tests.py
@@ -24,7 +24,7 @@ class TestModels(TestCase):
 class TestViews(TestCase):
     def test_expense_create(self):
         payload = {
-            "amount": 50,
+            "amount": 50.0,
             "merchant": "AT&T",
             "description": "cell phone subscription",
             "category": "utilities",
@@ -34,7 +34,7 @@ class TestViews(TestCase):
         )
         self.assertEqual(201, res.status_code)
         json_res = res.json()
-        self.assertEqual(str(payload["amount"]), json_res["amount"])
+        self.assertEqual(payload["amount"], json_res["amount"])
         self.assertEqual(payload["merchant"], json_res["merchant"])
         self.assertEqual(payload["description"], json_res["description"])
         self.assertEqual(payload["category"], json_res["category"])
@@ -47,3 +47,39 @@ class TestViews(TestCase):
         self.assertIsInstance(json_res, list)
         expenses = models.Expense.objects.all()
         self.assertEqual(len(expenses), len(json_res))
+
+    def test_expense_create_required_fields_missing(self):
+        payload = {
+            "merchant": "AT&T",
+            "description": "cell phone subscription",
+            "category": "utilities",
+        }
+        res = self.client.post(
+            reverse("restapi:expense-list-create"), payload, format="json"
+        )
+        self.assertEqual(400, res.status_code)
+
+    def test_expense_retrieve(self):
+        expense = models.Expense.objects.create(
+            amount=300, merchant="George", description="loan", category="transfer"
+        )
+        res = self.client.get(
+            reverse("restapi:expense-retrieve-delete", args=[expense.id]), format="json"
+        )
+        self.assertEqual(200, res.status_code)
+        json_res = res.json()
+        self.assertEqual(expense.id, json_res["id"])
+        self.assertEqual(expense.amount, json_res["amount"])
+        self.assertEqual(expense.merchant, json_res["merchant"])
+        self.assertEqual(expense.description, json_res["description"])
+        self.assertEqual(expense.category, json_res["category"])
+
+    def test_expense_delete(self):
+        expense = models.Expense.objects.create(
+            amount=400, merchant="John", description="loan", category="transfer"
+        )
+        res = self.client.delete(
+            reverse("restapi:expense-retrieve-delete", args=[expense.id]), format="json"
+        )
+        self.assertEqual(204, res.status_code)
+        self.assertFalse(models.Expense.objects.filter(pk=expense.id).exists())
--- restapi/urls.py
+++ restapi/urls.py
@@ -2,4 +2,11 @@
 from restapi import views
 from django.urls import path, include
 
-urlpatterns = [path("expenses/", views.ExpenseListCreate.as_view(), name="expense-list-create")]
+urlpatterns = [
+    path("expenses/", views.ExpenseListCreate.as_view(), name="expense-list-create"),
+    path(
+        "expenses/<pk>",
+        views.ExpenseRetrieveDelete.as_view(),
+        name="expense-retrieve-delete",
+    ),
+]
